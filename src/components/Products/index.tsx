21e4c343cc9cf680b620b33caaf9f8eff49d06b6
Bruna Soncini <brusoncini@gmail.com>
2024-02-04 19:58:19 -0300

    slider configurations

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import productList from "../../data/productList.js";
+import Slider from "react-slick";
+import leftArrow from './img/LeftArrow.svg'
+import rightArrow from './img/RightArrow.svg'
 import "./style.scss";
 
 interface Product {
@@ -9,27 +12,80 @@ interface Product {
   price: number;
 }
 
-function Products() {
+const PrevArrow = (props: any) => {
+  const { onClick } = props;
   return (
-    <section>
-      <div className="products-container">
-        {productList.map((product: Product) => (
-          <div className="product-list" key={product.productName}>
-            <img src={product.photo} alt={product.descriptionShort} />
-            <p className="description">{product.descriptionShort}</p>
-            <p className="original-price">
-              R$ {product.price.toLocaleString("pt-BR")}
-            </p>
-            <p className="price">R$ {product.price.toLocaleString("pt-BR")}</p>
-            <p className="half-price">
-              ou 2x de R$ {(product.price * 0.5).toLocaleString("pt-BR")} sem
-              juros
-            </p>
-            <span className="shipping">Frete grátis</span>
-            <button>COMPRAR</button>
+    <button className="arrow prev" onClick={onClick}>
+      <img src={leftArrow} alt="Previous" />
+    </button>
+  );
+};
+
+const NextArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <button className="arrow next" onClick={onClick}>
+      <img src={rightArrow} alt="Next" />
+    </button>
+  );
+};
+
+function Products() {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
+  return (
+    <section className="product-list">
+      
+      <Slider {...settings}>
+        {productList.map((product: Product, index: number) => (
+          <div className="product-container">
+            <div className="product-item" key={index}>
+              <img 
+              src={product.photo} 
+              alt={product.descriptionShort} 
+              />
+              <p className="description">
+                {product.descriptionShort}
+              </p>
+              <p className="discount">
+                R$ {product.price.toLocaleString("pt-BR")}
+              </p>
+              <p className="price">
+                R$ {product.price.toLocaleString("pt-BR")}
+              </p>
+              <p className="half-price">
+                ou 2x de R$ {(product.price * 0.5).toLocaleString("pt-BR")} sem
+                juros
+              </p>
+              <span className="shipping">
+                Frete grátis
+              </span>
+              <button>COMPRAR</button>
+            </div>
           </div>
         ))}
-      </div>
+      </Slider>
     </section>
   );
 }
